1571beaaebaf9822703ed2417cc8bcc05c8695e7
Micha Buchholz <mail@lean-stack.rocks>
2024-08-02 17:38:41 +0200

    chore: Create project. Initial commit.

---
+++ src/button/__tests__/button.stories.tsx
@@ -0,0 +1,4 @@
+import type { Story } from '@ladle/react';
+import { Button } from '../button';
+
+export const Default: Story = () => <Button>Click me!</Button>;
---
+++ src/button/button.tsx
@@ -0,0 +1,12 @@
+import {
+  Button as RACButton,
+  type ButtonProps as RACButtonProps,
+} from 'react-aria-components';
+
+namespace Button {
+  export interface Props extends RACButtonProps {}
+}
+
+export function Button({ ...props }: Button.Props) {
+  return <RACButton {...props} />;
+}
