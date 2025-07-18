569f4c43a49d4b76d1aca6944cdada7f71fea60f
Micha Buchholz <mail@lean-stack.rocks>
2025-07-19 00:38:35 +0200

    chore: Drop any tailwind config by now.

--- packages/core/react/src/components/button/button.tsx
+++ packages/core/react/src/components/button/button.tsx
@@ -1,43 +1,12 @@
 import {
   Button as RACButton,
   type ButtonProps as RACButtonProps,
-  composeRenderProps,
-} from 'react-aria-components';
-
-import { type VariantProps, tv } from 'tailwind-variants';
-import { focusRingStyles } from '../base-styles';
-
-const styles = tv({
-  extend: focusRingStyles,
-  base: [
-    'flex cursor-default items-center rounded-default px-4 py-2 font-semibold transition',
-    'data-[pressed=true]:scale-95',
-  ],
-  variants: {
-    color: {
-      default: 'bg-component text-component hover:bg-component-hover',
-      primary:
-        'bg-component-accent text-component-accent hover:bg-component-accent-hover',
-    },
-  },
-  defaultVariants: {
-    color: 'default',
-  },
-});
+} from "react-aria-components";
 
 namespace Button {
-  export interface Props
-    extends RACButtonProps,
-      Pick<VariantProps<typeof styles>, 'color'> {}
+  export interface Props extends RACButtonProps {}
 }
 
-export function Button({ className, color, ...props }: Button.Props) {
-  return (
-    <RACButton
-      className={composeRenderProps(className, (className, renderProps) =>
-        styles({ ...renderProps, color, className }),
-      )}
-      {...props}
-    />
-  );
+export function Button({ ...props }: Button.Props) {
+  return <RACButton {...props} />;
 }
--- packages/core/react/tsup.config.ts
+++ packages/core/react/tsup.config.ts
@@ -1,9 +1,9 @@
-import { defineConfig } from 'tsup';
+import { defineConfig } from "tsup";
 
 export default defineConfig({
-  entry: ['src/index.ts'],
-  format: ['esm'],
-  target: 'es2022',
+  entry: ["src/index.ts"],
+  format: ["esm"],
+  target: "es2022",
   dts: true,
   clean: true,
 });
